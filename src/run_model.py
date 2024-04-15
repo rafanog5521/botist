fe99c511a31dc990fa66cd4be84facf80e746e18
Rafael Noguera <162468418+rafanog5521@users.noreply.github.com>
2024-04-15 13:51:44 -0300

    added questions argument
    number of questions added as an argument to the run_models.py script

--- src/run_model.py
+++ src/run_model.py
@@ -7,7 +7,7 @@ import random
 
 parser = argparse.ArgumentParser(description='Run model')
 parser.add_argument('--init_only', help='Trigger a sample run to download requirements', required=False, action=argparse.BooleanOptionalAction)
-parser.add_arguument('-q', type=int, default=3, help='Amount of questions to evaulate the model(3 by default)', required=False)
+parser.add_argument('-q', '--questions', type=int, default=3, help='Amount of questions to evaluate the model(3 by default)', required=False)
 args = vars(parser.parse_args())
 
 if 'TinyLlama' in parameters.model:
@@ -16,17 +16,19 @@ if 'TinyLlama' in parameters.model:
         interactor.init_model()
     else:
         with open(parameters.questions_path, 'r') as file:
-        full_questionnaire = json.load(file)
-        # Load a random subset of n questions to execute the tests
-        question_numbers = [random.randint(0, len(full_questionnaire)) for _ in range(question_amount)]
-        questionnaire = []
-        for n in question_numbers:
-            questionnaire.append(full_questionnaire[n])
+            full_questionnaire = json.load(file)
+            # Load a random subset of n questions to execute the tests
+            question_numbers = [random.randint(0, len(full_questionnaire)) for _ in range(args["questions"])]
+            questionnaire = []
+            for n in question_numbers:
+                questionnaire.append(full_questionnaire[n])
 
-        progress_bar = tqdm(total=len(questionnaire), desc="Processing questionnaire...")
+            progress_bar = tqdm(total=len(questionnaire), desc="Processing questionnaire...")
 
-        for q in questionnaire:
-            resp = interactor.ask_question(question=q)
-            q.update({"response": resp})
-            progress_bar.update(1)
-        progress_bar.close()
+            for q in questionnaire:
+                resp = interactor.ask_question(question=q)
+                q.update({"response": resp})
+                progress_bar.update(1)
+            progress_bar.close()
+
+            print(questionnaire)
