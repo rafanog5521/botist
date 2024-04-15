79a6a11f271ac820c906c7166dea9cbede8f607d
Rafael Noguera <162468418+rafanog5521@users.noreply.github.com>
2024-04-15 11:37:53 -0300

    added raw metrics for performance by default

--- src/model_interactor.py
+++ src/model_interactor.py
@@ -3,10 +3,9 @@ import transformers
 from transformers import pipeline
 import time
 import jiwer
-import argparse
 
 
-class TinyLlamaModelInteractor():
+class TinyLlamaModelInteractor:
     def __init__(self, answers=parameters.num_return_sequences):
         self.__pipe__ = pipeline(task=parameters.task, model=parameters.model,
                                  torch_dtype=parameters.torch_dtype, device_map=parameters.device_map,
@@ -18,10 +17,24 @@ class TinyLlamaModelInteractor():
         return self.__pipe__.tokenizer.apply_chat_template(new_q, tokenize=parameters.tokenize,
                                                            add_generation_prompt=parameters.add_generation_prompt)
 
-    def ask_question(self, question):
+    def ask_question(self, question, performance_metric=True):
         prompt = self.prompt(question)
+        if performance_metric:
+            start_time = time.time()
+
         output =  self.__pipe__(prompt, max_new_tokens=parameters.max_new_tokens, do_sample=parameters.do_sample,
                                 temperature=parameters.temperature, top_k=parameters.top_k, top_p=parameters.top_p)
+        response = {"output": output[0]["generated_text"].split('<|assistant|>\n')[1]}
 
-        return output[0]["generated_text"]  # .split('<|assistant|>\n')
+        if performance_metric:
+            end_time = time.time()
+            # Calculate response time
+            response_time = end_time - start_time
+            response.update({"response_time": response_time})
+            # Calculate tokens per second
+            total_tokens_generated = len(output[0]["generated_text"])
+            tokens_per_second = total_tokens_generated / response_time
+            response.update({"tokens_per_second": tokens_per_second})
+
+        return response
 
--- src/run_model.py
+++ src/run_model.py
@@ -2,14 +2,28 @@
 from model_interactor import TinyLlamaModelInteractor
 from config import parameters
 import json
+from tqdm import tqdm
+import random
+
+question_amount = 3
 
 if 'TinyLlama' in parameters.model:
     interactor = TinyLlamaModelInteractor()
 
     question = open(parameters.questions_path, 'r')
-    
+
     with open(parameters.questions_path, 'r') as file:
-        questionnaire = json.load(file)
+        full_questionnaire = json.load(file)
+        # Load a random subset of n questions to execute the tests
+        question_numbers = [random.randint(0, len(full_questionnaire)) for _ in range(question_amount)]
+        questionnaire = []
+        for n in question_numbers:
+            questionnaire.append(full_questionnaire[n])
+
+        progress_bar = tqdm(total=len(questionnaire), desc="Processing questionnaire...")
+
         for q in questionnaire:
-            print("\n")
-            print(interactor.ask_question(question=q))
+            resp = interactor.ask_question(question=q)
+            q.update({"response": resp})
+            progress_bar.update(1)
+        progress_bar.close()
