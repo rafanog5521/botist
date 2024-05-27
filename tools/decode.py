8903d18a739d409393bf314cb807f8d3620abf7b
Rafael Noguera <162468418+rafanog5521@users.noreply.github.com>
2024-05-27 09:55:50 -0300

    changes required due to difference in register audio file path and extracted dataset
    There's currently a difference between the audio file path included in each example in the librispeech dataset

--- tools/decode.py
+++ tools/decode.py
@@ -1,2 +1,47 @@
 ######
-# Dado un archivo, verificar y calcular el WER de 
+# Dado un archivo, verificar y calcular el WER de la tokenizacion de un dataset tokenizado
+from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor
+from datasets import load_dataset
+import argparse
+import logging
+import json
+
+MODEL = "facebook/wav2vec2-base-960h"
+
+def decode_tokens(rosetta, token_array):
+    for t in token_array:
+        translation = []
+        print(f"Decoding: {t}")
+        for token in t.values:
+            print(token)
+        input()
+
+def decode_rosetta(stone):
+    inverted_dict = {}
+    for key, value in stone.items():
+        if value not in inverted_dict:
+            inverted_dict[value] = key
+        else:
+            inverted_dict[value].append(key)
+    return inverted_dict
+
+def main():
+    parser = argparse.ArgumentParser(description=f"Tool to tokenize audio dataset using {MODEL}")
+    parser.add_argument("-d", "--dataset", type=str, required=True, help="Name of the dataset to load")
+    parser.add_argument("-s", "--subset", type=str, required=True, help="Subset of the dataset to load")
+    parser.add_argument("-p", "--split", type=str, required=True, help="Split of the dataset to load (e.g., train, validation, test)")
+    parser.add_argument("-f", "--file", type=str, required=True, help="Path to file with the tokens")
+    parser.add_argument("-v", "--verbose", action="store_true", help="Verbose mode")
+    args = parser.parse_args()
+
+    rosetta = decode_rosetta(Wav2Vec2Processor.from_pretrained(MODEL).tokenizer.vocab)
+    raw_ds = load_dataset(args.dataset, args.subset, split=args.split, trust_remote_code=True)
+    with open(args.file, "r") as file:
+        tokens = json.load(file)
+    
+    decode_tokens(rosetta, tokens)
+    
+    
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.ERROR)
+    main()
--- tools/tokenizer.py
+++ tools/tokenizer.py
@@ -5,37 +5,97 @@ from tqdm import tqdm
 import json
 import torch
 import argparse
+import torchaudio
 import librosa
 import logging
 import concurrent.futures
+import soundfile as sf
 
 MODEL = "facebook/wav2vec2-base-960h"
 SAMPLING_RATE = 16000
+EXTRACTED_PATH_FRACTION = "/Librispeech/train-clean-360" # this is to cover the difference between the theoretical path and actual path after extraction
 
 def load_audio(audio_path):
-    audio, sr = librosa.load(audio_path, sr=SAMPLING_RATE)  # Sampling rate = 16kHz
-    return audio
+    try:
+        waveform, sr = torchaudio.load(audio_path)
+        if sr != SAMPLING_RATE:
+            waveform = torchaudio.transforms.Resample(sr, SAMPLING_RATE)(waveform)
+        return waveform.squeeze().numpy()
+    except Exception as e:
+        logging.error(f"torchaudio failed to load {audio_path} with error: {e}")
+        try:
+            audio, sr = librosa.load(audio_path, sr=SAMPLING_RATE)
+            return audio
+        except Exception as e:
+            logging.error(f"librosa also failed to load {audio_path} with error: {e}")
+            try:
+                audio, sr = sf.read(audio_path)
+                if sr != SAMPLING_RATE:
+                    audio = librosa.resample(audio, sr, SAMPLING_RATE)
+                return audio
+            except Exception as e:
+                logging.error(f"soundfile also failed to load {audio_path} with error: {e}")
+                raise
 
 def get_tokens_from_audio(audio_path):
     audio = load_audio(audio_path)
-    input_values = processor(audio, return_tensors="pt", padding="longest", sampling_rate=SAMPLING_RATE).input_values.to(device)  
+    input_values = processor(audio, return_tensors="pt", padding="longest", sampling_rate=SAMPLING_RATE).input_values.to(device)
     with torch.no_grad():
         logits = model(input_values).logits
-    
+
     predicted_ids = torch.argmax(logits, dim=-1)
 
     return predicted_ids
 
-def process_entry(d):
-    e_text = d["text"]
-    audio_path = d["audio"]["path"]
-    tokens = get_tokens_from_audio(audio_path)
-    raw_tokens = json.dumps(tokens[0].cpu().numpy().tolist()) 
-    json_entry = {
-        "text": e_text,
-        d["id"]: raw_tokens
-    }
-    return json_entry
+def process_entry(entry):
+    results = []
+    try:
+        if not all(key in entry for key in ["text", "audio"]):
+            raise KeyError(f"Missing keys in entry: {entry}")
+        ## this next snippet need to be implemented due to a recent change in the structure how the dataset is extracted
+        audio_path = entry["audio"]["path"]
+        entry_id_path = EXTRACTED_PATH_FRACTION + str(entry["speaker_id"]) + "/" + str(entry["chapter_id"])
+        audio_path = audio_path[:audio_path.rfind("/")] + entry_id_path + audio_path[audio_path.rfind("/"):]
+        tokens = get_tokens_from_audio(audio_path)
+        raw_tokens = tokens[0].cpu().numpy().tolist()
+        json_entry = {
+            entry["id"]: raw_tokens
+        }
+        results.append(json_entry)
+    except Exception as e:
+        logging.error(f"Exception: {e} in entry {entry}")
+    return results
+
+def process_ds(args, ds):
+    progress_bar = tqdm(total=len(ds), desc="Generating tokens:")
+    token_list = []
+
+    num_batches = (len(ds) + args.batch_size - 1) // args.batch_size 
+    for i in range(num_batches):
+        batch_start = i * args.batch_size
+        batch_end = min((i + 1) * args.batch_size, len(ds))
+        batch = ds[batch_start:batch_end]
+
+        # To reconstruct the dictionaries
+        batch_entries = [dict(zip(batch.keys(), values)) for values in zip(*batch.values())]
+
+        with concurrent.futures.ThreadPoolExecutor(max_workers=args.threads) as executor:
+            futures = [executor.submit(process_entry, entry) for entry in batch_entries]
+            for future in concurrent.futures.as_completed(futures):
+                try:
+                    json_entries = future.result()
+                    if json_entries:
+                        token_list.extend(json_entries)
+                        if args.verbose:
+                            for entry in json_entries:
+                                print(f"\n{entry}")
+                except Exception as exc:
+                    logging.error(f"Generated an exception: {exc}")
+
+            progress_bar.update(len(batch_entries))
+
+    progress_bar.close()
+    return token_list
 
 def main():
     parser = argparse.ArgumentParser(description=f"Tool to tokenize audio dataset using {MODEL}")
@@ -43,10 +103,11 @@ def main():
     parser.add_argument("-s", "--subset", type=str, required=True, help="Subset of the dataset to load")
     parser.add_argument("-p", "--split", type=str, required=True, help="Split of the dataset to load (e.g., train, validation, test)")
     parser.add_argument("-v", "--verbose", action="store_true", help="Verbose mode")
-    parser.add_argument("-t", "--threads", type=int, default=4, help="Number of threads to use for parallel processing")  
+    parser.add_argument("-t", "--threads", type=int, default=4, help="Number of threads to use for parallel processing")
+    parser.add_argument("--batch_size", type=int, default=10, help="Batch size for processing")
     args = parser.parse_args()
 
-    global processor  # Both processor and model need to be global so that it can be accessible for subprocess
+    global processor
     processor = Wav2Vec2Processor.from_pretrained(MODEL)
     global model
     model = Wav2Vec2ForCTC.from_pretrained(MODEL)
@@ -55,25 +116,13 @@ def main():
     model.to(device)
 
     ds = load_dataset(args.dataset, args.subset, split=args.split, trust_remote_code=True)
+    ds_path = ds[0]["file"].split("/")[8]
+    
     print(f"\n* Working with {MODEL} to tokenize...")
-    progress_bar = tqdm(total=len(ds), desc="Generating tokens:")
-    token_list = []
 
-    with concurrent.futures.ThreadPoolExecutor(max_workers=args.threads) as executor:
-        futures = {executor.submit(process_entry, d): d for d in ds}
-        for future in concurrent.futures.as_completed(futures):
-            try:
-                json_entry = future.result()
-                token_list.append(json_entry)
-                if args.verbose:
-                    print(f"\n{json_entry}")
-            except Exception as exc:
-                print(f"Generated an exception: {exc}")
-            progress_bar.update(1)
-
-    progress_bar.close()
+    token_list = process_ds(args, ds)
     rep = Reporter(args)
-    rep.dump_info(token_list, "tokenized_output", rep.create_report_folder())
+    rep.dump_info(token_list, args.subset + "_" + args.split, rep.create_report_folder())
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.ERROR)
